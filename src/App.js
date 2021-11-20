ea55228bbc99a5db02b8fa8a7d63cc3a04779a44
Agustin Rezett <nazareno.agustin45@gmail.com>
2021-11-20 10:21:26 -0300

    FEAT - Estructura de react-router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,47 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+/* eslint-disable no-sequences */
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
 
 // Components
 
 // Hooks
+import { NavbarContext } from './hooks/useContext/NavbarContext';
 
 // Styles
 import './css/global.css';
 
 // Navigation
+import Home from './components/pages/Home';
+import Login from './components/pages/Login';
 
-function App() {
-	return <div></div>;
+export default function App() {
+	const [activeSidebar, setActiveSidebar] = useState(false);
+	const [homePageLocation, setHomePageLocation] = useState(null);
+	const [searchValue, setSearchValue] = useState('');
+	const [showNavbar, setShowNavbar] = useState(false);
+
+	return (
+		<Router basename="/">
+			<NavbarContext.Provider
+				value={{
+					activeSidebar,
+					setActiveSidebar,
+					searchValue,
+					setSearchValue,
+					homePageLocation,
+				}}
+			>
+				{/* {showNavbar && <TopNavbar />}
+				{showNavbar && <SideNavbar />} */}
+				<NavLink to="/home">Home</NavLink>
+				<NavLink to="/login">Login</NavLink>
+			</NavbarContext.Provider>
+			<Routes>
+				<Route path="/home" element={<Home />} />
+				{/* <Route path="/" exact>
+					<Login hideNavbar={() => setShowNavbar(false)} />
+				</Route> */}
+			</Routes>
+		</Router>
+	);
 }
-
-export default App;
