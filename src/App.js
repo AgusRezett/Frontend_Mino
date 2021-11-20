0867db03551fb8b452120a183e7c5a6494121946
Agustin Rezett <nazareno.agustin45@gmail.com>
2021-11-19 21:59:22 -0300

    FEAT - Limpieza de espacio de trabajo

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+// Components
+
+// Hooks
+
+// Styles
+import './css/global.css';
+
+// Navigation
+
+function App() {
+	return <div></div>;
+}
+
+export default App;
