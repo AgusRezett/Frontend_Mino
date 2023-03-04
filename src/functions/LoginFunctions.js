dab4496b76f1285ceb07a4a262cecb6643385f83
Agustin Rezett <agustin.rezett@gmail.com>
2023-03-04 17:41:31 -0300

    STASH

--- src/functions/LoginFunctions.js
+++ src/functions/LoginFunctions.js
@@ -1,18 +1,18 @@
 //? Return if the user has already entered to this page before
 export const getFirstTime = () => {
-	let firstTime = localStorage.getItem('first-time');
-	if (firstTime === null) {
-		firstTime = true;
-		localStorage.setItem('first-time', firstTime);
-	} else {
-		firstTime = false;
-	}
-	return firstTime;
+    let firstTime = localStorage.getItem('first-time');
+    if (firstTime === null) {
+        firstTime = true;
+        localStorage.setItem('first-time', firstTime);
+    } else {
+        firstTime = false;
+    }
+    return firstTime;
 };
 
-//? Return if it's found a user session
+//? Return TRUE if it's found a user session
 export const getSession = () => {
-	let session = localStorage.getItem('user-token');
-	session === null ? (session = true) : (session = false);
-	return session;
+    let session = localStorage.getItem('user-token');
+    session === null ? (session = true) : (session = false);
+    return session;
 };
